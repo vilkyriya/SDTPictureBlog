41c314f9692aa4a9999908ad0789172eb65b2f77
vilkyriya <vilkyriya@gmail.com>
2020-06-19 00:14:35 +0700

    Автоматизация сборки Автоматизация сборки CSS файлов с помощью инструмента Laravel Mix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,9 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <meta content='width=device-width, initial-scale=1, maximum-scale=1' name='viewport'/>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/bootswatch/4.3.1/lux/bootstrap.min.css" rel="stylesheet" />
+    <link rel="stylesheet" type="text/css" href="{{ mix('/css/app.css') }}">
 
 </head>
 <body>
